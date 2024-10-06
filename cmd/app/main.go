6742cff35e71523dcc9f2da207b9cf384e709eea
Ayush Chadha <elegantshock3307@gmail.com>
2024-10-07 00:37:53 +0530

    cleanup: .env setup for server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"github.com/ChickenWhisky/makeItIntersting/internals/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Get host and port from environment variables
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	address := host + ":" + port
+
 	// Initialize the Gin router
 	router := gin.Default()
 
 	// Set up routes from handlers package
 	handlers.SetupRoutes(router)
 
-	if err := router.Run(":8080"); err != nil {
+	// Run the server
+	if err := router.Run(address); err != nil {
 		log.Fatalf("Failed to run the server: %v", err)
 	}
 }
